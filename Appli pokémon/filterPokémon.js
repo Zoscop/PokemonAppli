9f361d97483e5f9c3d73922e4837e125f145d5dd
Zoscop <enzobertrand47@gmail.com>
2025-02-07 17:08:38 +0100

    Add files via upload

---
+++ Appli pokémon/filterPokémon.js
@@ -0,0 +1,53 @@
+const urlParams = new URLSearchParams(window.location.search)
+const selectedType = urlParams.get("type")
+const filteredPokemonsContainer = document.getElementById("filteredPokemonsContainer")
+const apiUrl = "https://pokeapi.co/api/v2/"
+
+if (!selectedType) {
+    filteredPokemonsContainer.innerHTML = "<p>Veuillez sélectionner un type.</p>"
+} else {
+    fetchPokemonsByType()
+}
+
+//Récupération des pokémon en fonction du type séléctionné
+async function fetchPokemonsByType() {
+    try {
+        filteredPokemonsContainer.innerHTML = "<p>Chargement des Pokémon...</p>"
+
+        const response = await fetch(`${apiUrl}type/${selectedType}`)
+        if (!response.ok) throw new Error("Erreur lors de la récupération des Pokémon du type.")
+        const data = await response.json()
+
+        if (data.pokemon.length === 0) {
+            filteredPokemonsContainer.innerHTML = "<p>Aucun Pokémon trouvé pour ce type.</p>"
+            return
+        }
+
+        
+     // Affichage des Pokémon du type sélectionné avec leur nom et une photo
+    filteredPokemonsContainer.innerHTML = ""
+    for (let i = 0; i < Math.min(150, data.pokemon.length); i++) { // Limite à 150 Pokémons
+        const pokemon = data.pokemon[i].pokemon
+        const pokemonData = await fetch(`${apiUrl}pokemon/${pokemon.name}`).then(res => res.json())
+
+     const pokemonDiv = document.createElement("div")
+        pokemonDiv.innerHTML = `
+            <p>${pokemonData.name.toUpperCase()}</p>
+            <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="clickable-pokemon" data-name="${pokemonData.name}">
+        `
+        filteredPokemonsContainer.appendChild(pokemonDiv)
+    }
+
+    // Redirection vers la page principale avec le nom du pokémon filtré en paramètre
+    document.querySelectorAll(".clickable-pokemon").forEach(img => {
+        img.addEventListener("click", (e) => {
+            const selectedPokemon = e.target.getAttribute("data-name")
+            window.location.href = `indexPokémon.html?pokemon=${selectedPokemon}`
+        })
+    })
+    }catch (error) {
+        console.error(error);
+        filteredPokemonsContainer.innerHTML = "<p>Une erreur est survenue lors du chargement.</p>"
+}}
+
+
---
+++ Appli pokémon/scriptPokémon.js
@@ -0,0 +1,210 @@
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+const p = document.getElementById("p")
+const img = document.getElementById("img")
+const img2 = document.getElementById("back")
+const infoButton = document.getElementById("infoButton")
+const recommendedContainer = document.getElementById("recommendedContainer")
+const recentContainer = document.getElementById("recentContainer")
+const typeFilter = document.getElementById("typeFilter")
+const genFilter = document.getElementById("genFilter")
+const filterButton = document.getElementById("filterButton")
+
+const url = "https://pokeapi.co/api/v2/"
+let recentSearches = []
+let currentPokemon = null // Pokémon sélectionné
+
+// événement de recherche
+button.addEventListener("click", () => fetchPokemon(input.value.toLowerCase()))
+
+
+window.onload = async () => {
+    await populateFilters() // Chargement des types pour le filtrage
+    showRecommendedPokemons()  // Afficher les 5 pokémons recommandés
+    
+    // Vérifier si un pokémon est dans l'url
+    const urlParams = new URLSearchParams(window.location.search)
+    const pokemonName = urlParams.get("pokemon")
+
+    if (pokemonName) {
+        fetchPokemon(pokemonName) // Affichage des informations du pokémon
+    }
+    // Supprime le pokémon à la fin de l'url
+    setTimeout(() => {
+        window.history.replaceState({}, document.title, window.location.pathname)
+    }, 1000) 
+}
+
+
+
+
+// Récupération des données sur un pokémon
+async function fetchPokemon(pokemonName) {
+    if (!pokemonName) return
+
+    const response = await fetch(`${url}/pokemon/${pokemonName}`)
+    if (!response.ok) {
+        p.textContent = `Aucun Pokémon Trouvé`
+        img.setAttribute("src", "")
+        img2.setAttribute("src", "")
+        infoButton.style.display = "none"
+        return
+    }
+
+    const pokemon = await response.json()
+    currentPokemon = pokemon
+    let types = pokemon.types.map(t => t.type.name).join(", ")
+    p.textContent = `Le nom du Pokémon est ${pokemon.name} de type : ${types}`
+    img.setAttribute("src", pokemon.sprites.front_default)
+    img2.setAttribute("src", pokemon.sprites.back_default)
+    updatePokemonCry(pokemon.id) // Appelle la fonction pour le crie
+    updateRecentSearches(pokemon.name) //Appelle la fonction pour l'historique
+    infoButton.style.display = "block"
+}
+
+
+// Mise à jour de l'historique des recherches
+function updateRecentSearches(name) {
+    if (recentSearches.includes(name)) {
+        recentSearches = recentSearches.filter(n => n !== name)
+    }
+    recentSearches.unshift(name)
+    if (recentSearches.length > 5) {
+        recentSearches.pop()
+    }
+
+    recentContainer.innerHTML = ""
+    recentSearches.forEach(pokemon => {
+        const btn = document.createElement("button")
+        btn.textContent = pokemon
+        btn.addEventListener("click", () => fetchPokemon(pokemon))
+        recentContainer.appendChild(btn)
+    });
+}
+
+// Récupéreration de l'URL du cri d'un pokémon via l'API
+async function fetchPokemonCry(pokemonId) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`
+    try {
+        const response = await fetch(apiUrl)
+        if (!response.ok) {
+            throw new Error("Erreur lors de la récupération du cri du Pokémon.")
+        }
+
+
+        // Vérifier si un cri est disponible pour ce pokémon
+        const cryUrl = `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonId}.ogg`
+        return cryUrl
+    } catch (error) {
+        console.error("Erreur: ", error)
+    }
+}
+
+// Mise à jour de la source du cri
+async function updatePokemonCry(pokemonId) {
+    const cryUrl = await fetchPokemonCry(pokemonId);
+    if (cryUrl) {
+        // Mise à jour de la source du fichier audio
+        const audioElement = document.getElementById("cries");
+        const sourceElement = document.getElementById("cries-source");
+        sourceElement.src = cryUrl;  // Affectation de la nouvelle source audio
+        audioElement.load();  // Recharge l'élément audio avec la nouvelle source
+    }
+}
+
+
+// Affichage de "plus d'informations" dans une fenêtre modale
+infoButton.addEventListener("click", () => {
+    if (currentPokemon) {
+        showModal(currentPokemon)
+    }
+});
+
+// Ouvre la fenêtre modale avec les infos du pokémon
+function showModal(pokemon) {
+    let modal = document.createElement("div")
+    modal.style.position = "fixed"
+    modal.style.top = "50%"
+    modal.style.left = "50%"
+    modal.style.transform = "translate(-50%, -50%)"
+    modal.style.padding = "20px"
+    modal.style.background = "white"
+    modal.style.boxShadow = "0px 0px 10px #000000"
+    modal.style.zIndex = "1000"
+    
+    // Peut être fait à part
+    modal.innerHTML = `
+        <h3>${pokemon.name.toUpperCase()}</h3>
+        <p>Type: ${pokemon.types.map(t => t.type.name).join(", ")}</p>
+        <p>Poids: ${pokemon.weight / 10} kg</p>
+        <p>Taille: ${pokemon.height / 10} m</p>
+        <p>Talents : ${pokemon.abilities.map(t => t.ability.name).join(", ")}</p>
+        <p>Version Shiny:</p>
+        <img src = ${pokemon.sprites.front_shiny}></img><img src = ${pokemon.sprites.back_shiny}></img>
+    `
+        
+    // Bouton "Fermer" pour fermer la fenêtre modale
+    const closeButton = document.createElement("button");
+    closeButton.textContent = "Fermer"
+    closeButton.style.marginTop = "10px"
+    closeButton.addEventListener("click", () => modal.remove())
+    modal.appendChild(closeButton)
+    document.body.appendChild(modal)
+}
+
+// Affichage des 5 pokémons recommandés aléatoirement
+async function showRecommendedPokemons() {
+    recommendedContainer.innerHTML = ""
+    const recommendedPokemons = await getRandomPokemons()
+    recommendedPokemons.forEach(pokemon => {
+        const btn = document.createElement("button")
+        btn.textContent = pokemon.name
+        btn.addEventListener("click", () => fetchPokemon(pokemon.name))
+        recommendedContainer.appendChild(btn)
+    })
+}
+
+// Récupérartion des pokémons aléatoires
+async function getRandomPokemons() {
+    const randomPokemons = []
+    
+    while (randomPokemons.length < 5) {
+        const randomId = Math.floor(Math.random() * 1000) + 1
+        const response = await fetch(`${url}/pokemon/${randomId}`)
+        if (response.ok) {
+            const pokemon = await response.json()
+            if (!randomPokemons.find(p => p.name === pokemon.name)) {
+                randomPokemons.push(pokemon)
+            }
+        }
+    }
+    
+    return randomPokemons
+}
+
+// Chargement des types dans le filtre
+async function populateFilters() {
+    const typesResponse = await fetch(`${url}/type`)
+    const typesData = await typesResponse.json()
+    typesData.results.forEach(type => {
+        const option = document.createElement("option")
+        option.value = type.name
+        option.textContent = type.name.charAt(0).toUpperCase() + type.name.slice(1)
+        typeFilter.appendChild(option)
+    })
+
+}
+
+// événement de clic pour le bouton de filtrage
+filterButton.addEventListener("click", () => {
+    const selectedType = typeFilter.value
+
+    // Vérification que le type est sélectionné
+    if (selectedType) {
+        // Construction de l'URL avec les paramètres
+        const url = `filterPokémon.html?type=${selectedType}`
+        window.location.href = url  // Redirection vers la page filtrée
+    } 
+})
+
+
