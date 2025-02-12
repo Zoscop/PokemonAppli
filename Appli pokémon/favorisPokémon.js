c05f8cc62a8e9d3582b45ca3cd0efad0df26795b
Zoscop <enzobertrand47@gmail.com>
2025-02-12 08:23:11 +0100

    Update favorisPokémon.js

--- Appli pokémon/favorisPokémon.js
+++ Appli pokémon/favorisPokémon.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const favPokemonContainer = document.getElementById("favPokemonContainer")
     const apiUrl = "https://pokeapi.co/api/v2/"
 
+    // Recherche de favoris dans le local storage, si oui, appel de la fonction
     if (favorites.length === 0) {
         favPokemonContainer.innerHTML = "<p>Aucun Pokémon en favori.</p>"
         return
@@ -10,12 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
         pokemonFav(favorites.name)
     }
 
+    // Fonction pour afficher les pokémons en favoris
     async function pokemonFav() {
         favPokemonContainer.innerHTML = ""
 
+        // Parcours du tableau local storage
         for (let i = 0; i < favorites.length; i++){
             const response = await fetch(`${apiUrl}/pokemon/${favorites[i]}`)
             const pokemon = await response.json()
+            
+            // Création des div dans le container
             const pokemonDiv = document.createElement("div")
             pokemonDiv.innerHTML = `
                 <p>${pokemon.name.toUpperCase()}</p>
@@ -23,7 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
         `
             favPokemonContainer.appendChild(pokemonDiv)
         }   
-        
+
+        // Redirection vers la page principale
         document.querySelectorAll(".clickable-pokemon").forEach(img => {
             img.addEventListener("click", (e) => {
                 const selectedPokemon = e.target.getAttribute("data-name")
